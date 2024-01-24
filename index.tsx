11dbbfa19c7ecb4ebdcfd000053cb6f82d2a1df4
vlasemkorm <vlasenkorm@gmail.com>
2024-01-24 14:28:45 +0200

    react change

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import App from "./src/App";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const root = createRoot(document.querySelector("#root")!); // createRoot(container!) if you use TypeScript
+root.render(<App/>);
